d6b7be7fcb0687399df00b6d25a1fe8a3c24dee4
Zahidul Haque <zahidulmia@users.noreply.github.com>
2020-07-09 01:14:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-
+const mongoose = require("mongoose");
 const app = express();
 app.set("view engine", "ejs");
 
-var items = [];
-var workitems = [];
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
 
+
+const todolistSchema = {
+  name: String
+};
+
+
+
+const Items = mongoose.model("Items", todolistSchema);
+
+const items1 = new Items({
+  name: "welcome "
+
+});
+const items2 = new Items({
+  name: "welcome 2"
+
+});
+const defaultItems = [items1, items2];
+
+Items.insertMany(defaultItems, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("success DB");
+  }
+
+});
 
 app.get("/", function(req, res) {
-let day = date.getDate();
+
 
   res.render("list", {
-    listtitle: day,
-newlistItem: items
+    listtitle: "Today"
+    newlistItem: Items
   });
 
 
@@ -27,9 +55,12 @@ newlistItem: items
 });
 app.get("/work", function(req, res) {
 
-res.render("list", {listtitle: "work" , newlistItem: workitems })
+  res.render("list", {
+    listtitle: "work",
+    newlistItem: workitems
+  })
 
-  });
+});
 
 
 
@@ -37,17 +68,17 @@ res.render("list", {listtitle: "work" , newlistItem: workitems })
 
 app.post("/", function(req, res) {
 
-console.log(req.body);
-  if(req.body.list === "work"){
+  console.log(req.body);
+  if (req.body.list === "work") {
     item = req.body.newItem;
     workitems.push(item);
-   res.redirect("/work");
- }else{
-   item = req.body.newItem;
-   items.push(item);
-  res.redirect("/");
+    res.redirect("/work");
+  } else {
+    item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 
- }
+  }
 
 
 
@@ -55,7 +86,7 @@ console.log(req.body);
 
 
 
-  app.listen(3000, function() {
+app.listen(3000, function() {
 
-    console.log("Server started on port 3000");
-  });
+  console.log("Server started on port 3000");
+});
